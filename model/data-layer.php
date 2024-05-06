021c03e1cce655c8bac714a473577099d149b200
gene7 <faison.eugene@student.greenriver.edu>
2024-05-06 15:22:38 -0400

    templating for mailing checkboxes

--- model/data-layer.php
+++ model/data-layer.php
@@ -3,3 +3,8 @@ function getJobs()
 {
     return array('JS', 'HTML', 'php', 'css', 'java', 'react', 'python', 'node');
 }
+
+function getVerticles()
+{
+    return array('SaaS', 'industrial', 'health', 'cyber', 'Ag', 'HR');
+}
